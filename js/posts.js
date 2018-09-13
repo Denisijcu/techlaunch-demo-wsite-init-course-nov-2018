6d1b1d9d0cf7fae99a517f04d2f935f5ec247d19
denisijcu <denisijcu266@gmail.com>
2018-09-13 11:15:32 -0400

    first commit

---
+++ js/posts.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyAhFQZfwxJxAilIUA-7Zh9ouhmVCVJRB7Q",
+    authDomain: "my-booklist-34f26.firebaseapp.com",
+    databaseURL: "https://my-booklist-34f26.firebaseio.com",
+    projectId: "my-booklist-34f26",
+    storageBucket: "my-booklist-34f26.appspot.com",
+    messagingSenderId: "740928828969"
+};
+
+firebase.initializeApp(config);
+
+console.log('firebase Init');
